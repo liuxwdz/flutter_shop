ab7adc9417858090c509117e376f533838112bd7
liuxw <liuxwdz@163.com>
2019-08-17 15:53:11 +0800

    网络请求，刷新界面

--- flutter_shop/lib/main.dart
+++ flutter_shop/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'pages/index_page.dart';
-import 'package:dio/dio.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    _getNews();
     return Container(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -18,16 +16,5 @@ class MyApp extends StatelessWidget {
         home: IndexPage(),
       ),
     );
-  }
-
-  void _getNews() async{
-    Response response;
-    try{
-      response = await Dio().get("http://api.wangshuwen.com/getNews");
-      return print(response);
-    }catch(e){
-      return print(e);
-    }
-
   }
 }
--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+}
+
+TextEditingController _editingController = TextEditingController();
+
+var textValue = "还没选择美女哦";
+
+class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("首页"),
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("天上人间"),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _editingController,
+                decoration: InputDecoration(
+                    labelText: "美女类型",
+                    helperText: "请选择美女类型",
+                    contentPadding: EdgeInsets.all(10.0)),
+              ),
+              RaisedButton(
+                onPressed: _doAction,
+                child: Text("确认选择"),
+              ),
+              Text(textValue)
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  void _doAction() {
+    print("开始拉去新闻");
+    if (_editingController.text.toString() == "") {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text("没有选择美女类型"),
+              ));
+    } else {
+      _getNews(_editingController.text.toString()).then((onValue) {
+        print(onValue);
+        setState(() {
+          textValue = onValue;
+        });
+      });
+    }
+  }
+
+  Future _getNews(String type) async {
+    print("你选择的是：$type");
+    var data = {"page": 1};
+    try {
+      Response response;
+      response = await Dio()
+          .get("http://api.wangshuwen.com/getNews", queryParameters: data);
+      return response.data["data"][0]["title"];
+    } catch (e) {
+      return print(e);
+    }
   }
 }
