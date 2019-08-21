047275a320b18f3b1cf28bc6e9c397ffe51dda7b
liuxiangwu <liuxw@hst.com>
2019-08-21 12:54:06 +0800

    引入Provide状态管理

--- flutter_shop/lib/main.dart
+++ flutter_shop/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'pages/index_page.dart';
+import 'provide/counter.dart';
+import 'package:provide/provide.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  final provide = Providers()
+    ..provide(Provider.function((context) => Counter(0)));
+  runApp(ProviderNode(child: MyApp(), providers: provide));
+}
 
 class MyApp extends StatelessWidget {
   @override
--- flutter_shop/lib/pages/cart_page.dart
+++ flutter_shop/lib/pages/cart_page.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provide/provide.dart';
+import '../provide/counter.dart';
 
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('购物车'),
+      ),
       body: Center(
-        child: Text("购物车"),
+        child: Column(
+          children: <Widget>[
+            Provide<Counter>(
+              builder: (context, child, counter) {
+                return Text('${counter.value}');
+              },
+            ),
+            RaisedButton(
+              onPressed: () {
+                Provide.value<Counter>(context).increment();
+              },
+              child: Text('自增加'),
+            )
+          ],
+        ),
       ),
     );
   }
